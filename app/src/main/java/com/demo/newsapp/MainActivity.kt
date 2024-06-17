586b4285ab7e492077362aa811c470c7fb25d4fc
supriya gupta <supriyag75@gmail.com>
2024-06-17 15:24:14 +0530

    Initial Commit

---
+++ app/src/main/java/com/demo/newsapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.demo.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            App()
+
+        }
+    }
+
+    @Composable
+    fun App() {
+        Text(text = "supriya")
+    }
+
+}
+
+
+
+
+
