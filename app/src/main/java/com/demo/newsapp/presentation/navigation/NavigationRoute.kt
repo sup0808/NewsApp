20035f6ec0d428697e56ee44edccdb1bd30e3d88
supriya gupta <supriyag75@gmail.com>
2024-06-17 18:39:14 +0530

    Add Navigation files

---
+++ app/src/main/java/com/demo/newsapp/presentation/navigation/NavigationRoute.kt
@@ -0,0 +1,29 @@
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+object MainDestination {
+    const val SPLASH_ROUTE = "splash"
+    const val HOME_ROUTE = "home"
+    const val LOGIN_ROUTE = "login"
+
+}
+
+@Composable
+fun NavigatorRoute() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = MainDestination.SPLASH_ROUTE){
+        composable(route= MainDestination.SPLASH_ROUTE){
+            SplashScreen(navController)
+        }
+        composable(route = MainDestination.LOGIN_ROUTE){
+            LoginScreen(navController)
+        }
+        composable(route = MainDestination.HOME_ROUTE){
+
+        }
+    }
+}
