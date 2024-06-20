1f94e90e47a8bbcbbd90fbd7bb10928edcf65009
supriya gupta <supriyag75@gmail.com>
2024-06-20 16:40:23 +0530

    Add 'remember me' checkbox -- add Forgot password -- add password with trailing icon for visiblity

--- app/src/main/java/com/demo/newsapp/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/demo/newsapp/presentation/login/LoginScreen.kt
@@ -1,10 +1,14 @@
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -16,17 +20,26 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+
+
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.IconButton
 
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -49,7 +62,6 @@ fun LoginScreen(navController: NavHostController) {
         Card(
             shape = RoundedCornerShape(10.dp),
             elevation = 10.dp,
-
             backgroundColor = Color.Gray,
             modifier = Modifier
                 .wrapContentWidth()
@@ -60,23 +72,34 @@ fun LoginScreen(navController: NavHostController) {
                 modifier = Modifier
                     .wrapContentWidth()
                     .wrapContentHeight()
-                    .padding(20.dp),
+                    .padding(40.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 UserNameField(
-                    value = "username", onChange = { }, modifier = Modifier.fillMaxWidth()
+                    value = "Username", onChange = { }, modifier = Modifier.fillMaxWidth()
                 )
-                UserNameField(
-                    value = "username", onChange = { }, modifier = Modifier.fillMaxWidth()
+                PasswordTextField(
+                    value = "Password", onChange = { }, modifier = Modifier.fillMaxWidth()
                 )
 
-                Button(onClick = {}, modifier = Modifier
-                    .wrapContentWidth()
-                    .width(100.dp), shape = RoundedCornerShape(5.dp), colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)) {
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .width(100.dp),
+                    shape = RoundedCornerShape(5.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)
+                ) {
                     Text(text = "Login")
                 }
+                Spacer(modifier = Modifier.height(10.dp))
 
+                LabeledCheckBox("Remember me",false,"Forgot password?"){
+
+                }
             }
 
         }
@@ -120,3 +143,74 @@ fun UserNameField(
     )
 
 }
+
+
+@Composable
+fun PasswordTextField(
+    value: String,
+    onChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    label: String = "Password",
+    placeholder: String = "Enter the password"
+) {
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    val leadingIcon = @Composable {
+        Icon(Icons.Default.Lock, contentDescription = "", tint = Color.LightGray)
+    }
+
+    val trailingIcon = @Composable {
+        IconButton(onClick = {
+            isPasswordVisible = !isPasswordVisible
+        }) {
+            Icon(if(isPasswordVisible) Icons.Default.Check else Icons.Default.Close
+                , contentDescription = "", tint = Color.LightGray)
+        }
+
+    }
+
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = onChange,
+        modifier = modifier,
+        leadingIcon = leadingIcon,
+        trailingIcon= trailingIcon,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next, keyboardType = KeyboardType.Text
+        ),
+        /* keyboardActions = KeyboardActions(
+             onNext = { focusManager.moveFocus(FocusDirection.Down) }
+         ),*/
+        placeholder = { Text(placeholder, style = TextStyle(color = Color.LightGray)) },
+        label = { Text(label, style = TextStyle(color = Color.DarkGray)) },
+        singleLine = true,
+        visualTransformation = VisualTransformation.None,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.DarkGray, textColor = Color.DarkGray
+        ),
+    )
+}
+
+@Composable
+fun LabeledCheckBox(
+    label: String,
+    isChecked : Boolean,
+    forgotPass : String,
+    onCheckChanged :() ->Unit
+
+){
+
+    Row(
+        modifier = Modifier.clickable {
+            onCheckChanged()
+        }) {
+        Checkbox(checked = isChecked, onCheckedChange = null)
+        Spacer(modifier = Modifier.width(5.dp))
+        Text(text = label, style = TextStyle(color = Color.DarkGray))
+        Spacer(Modifier.weight(1f))
+        Text(text = forgotPass, style = TextStyle(color = Color.DarkGray))
+    }
+
+}
+
